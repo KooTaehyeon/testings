7ec24ae9caeccb736191c3cf04dc76a05f9de355
Kth <vcro12123@naver.com>
2022-08-03 19:28:19 +0900

    버튼 style,text 테스트

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('button has correct initial color', () => {
   render(<App />);
-  const linkElement = screen.getByRole('link', { name: /learn react/i });
-  expect(linkElement).toBeInTheDocument();
+  // 버튼과 버튼에 해당 텍스트가 있는지 테스트
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+  // 버튼에 배경색을 확인하는 테스트
+  expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
 });
+
+test('button turns blue when clicked', () => {});
