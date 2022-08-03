bb5abc18a61e11e517800062b57f59b0a9599be2
Kth <vcro12123@naver.com>
2022-08-03 19:06:18 +0900

    getByRole

--- src/App.test.js
+++ src/App.test.js
@@ -2,5 +2,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  throw new Error('test failed');
+  render(<App />);
+  const linkElement = screen.getByRole('link', { name: /learn react/i });
+  expect(linkElement).toBeInTheDocument();
 });
